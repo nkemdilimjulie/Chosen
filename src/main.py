a0427b9a29660fcf9084b5748d41526e66ef5143
nkemdilim <nkemdilim.chime@dci-student.org>
2025-01-09 07:01:08 +0100

    adds a quit function

--- src/main.py
+++ src/main.py
@@ -1,64 +1,68 @@
-'''Main code operation'''
+"""Main code operation"""
+
 import time
 from game_describe import game_description
 from game_options import game_choice
 from roll_operations import roll_dice
 from roll_operations import roll_with_extra_dice
 from roll_operations import check_dice_rolled
+from roll_operations import ends_game
+
 
 def main():
-    '''dice game main'''
+    """dice game main"""
     game_description()
     game_choice()
     name = input("Please, enter your name: ")
-    print('Welcome to Dice Game !', name)
+    print("Welcome to Dice Game !", name)
     choice = 0
-    choice = int(input('Select your choice (1 - 3): '))
+    choice = int(input("Select your choice (1 - 3): "))
     if choice == 1:
         number_of_trials = 0
         remaining_trials = 6
         while remaining_trials <= 6:
             if remaining_trials == 0:
                 print("Sorry, your time is up")
-                print('Game over!')
-                play_again = input(f'{name}, Do you want to play again? ')
+                print("Game over!")
+                play_again = input(f"{name}, Do you want to play again? ")
                 if play_again in ["Yes", "yes", "Y", "y"]:
                     main()
                 elif play_again not in ["Yes", "yes", "Y", "y"]:
-                    break
+                    ends_game()
             number_of_trials += 1
             input("Press ENTER to roll dice: ")
             print(f"\nRound {number_of_trials}, rolling dice ...")
-            print('Your dice faces are:  ')
+            print("Your dice faces are:  ")
             dice_faces = roll_dice()
             time.sleep(1)
             check_dice_rolled(*dice_faces)
             remaining_trials = 6 - number_of_trials
-            print(f'You have {6-number_of_trials} more chance(s)')
+            print(f"You have {6-number_of_trials} more chance(s)")
     elif choice == 2:
         number_of_trials = 0
         remaining_trials = 6
         while remaining_trials <= 6:
             if remaining_trials == 0:
                 print("Sorry, your time is up")
-                print('Game over!')
-                play_again = input(f'{name}, Do you want to play again? ')
+                print("Game over!")
+                play_again = input(f"{name}, Do you want to play again? ")
                 if play_again in ["Yes", "yes", "Y", "y"]:
                     main()
                 elif play_again not in ["Yes", "yes", "Y", "y"]:
-                    break
+                    ends_game()
             number_of_trials += 1
             input("Press ENTER to roll dice: ")
             print(f"Round {number_of_trials}, rolling dice ...")
-            print('Your dice faces are:  ')
+            print("Your dice faces are:  ")
             dice_faces_extra = roll_with_extra_dice()
             time.sleep(1)
             check_dice_rolled(*dice_faces_extra)
             remaining_trials = 6 - number_of_trials
-            print(f'You have {6-number_of_trials} more chance(s)')
+            print(f"You have {6-number_of_trials} more chance(s)")
     elif choice == 3:
-            print('Game over')
+        print("Game over")
+        ends_game()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-   
--- src/roll_operations.py
+++ src/roll_operations.py
@@ -1,8 +1,11 @@
-'''Game Operations'''
+import sys
+
+"""Game Operations"""
 from create_dice import D4, D6, D8, D10
 
+
 def roll_dice():
-    '''rolls dice'''
+    """rolls dice"""
     d4 = D4()
     d6 = D6()
     d8 = D8()
@@ -10,8 +13,9 @@ def roll_dice():
     dice_faces = [d4, d6, d8]
     return dice_faces
 
+
 def roll_with_extra_dice():
-    '''rolls extra dice'''
+    """rolls extra dice"""
     d4 = D4()
     d6 = D6()
     d8 = D8()
@@ -20,13 +24,21 @@ def roll_with_extra_dice():
     dice_faces_extra = [d4, d6, d8, d10]
     return dice_faces_extra
 
+
 def check_dice_rolled(d4, d6, d8, d10=None):
-    '''checks for a winner'''
+    """checks for a winner"""
     if d4 == d6 and d6 == d8:
         print("Hurray! You've won the game!")
         print("... END OF GAME ...")
+        ends_game()
         if d10:
-            if d4 == d6 and d6 ==d8 and d8 == d10:
+            if d4 == d6 and d6 == d8 and d8 == d10:
                 print("Hurray! You've won the game with extra dice!")
                 print("... END OF GAME ...")
-   
+                ends_game()
+
+
+def ends_game():
+    """ends the game"""
+    print("Thanks for playing!")
+    sys.exit()
